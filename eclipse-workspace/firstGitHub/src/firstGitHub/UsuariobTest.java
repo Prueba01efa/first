565b3bdce0fda7c63bf03886a313c85c606314b8
= <andex91lpn2@gmail.com>
2020-05-10 13:58:51 +0200

    Java Doc 2 andexo

--- eclipse-workspace/firstGitHub/src/firstGitHub/UsuariobTest.java
+++ eclipse-workspace/firstGitHub/src/firstGitHub/UsuariobTest.java
@@ -5,11 +5,25 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
-
+/**
+ * Pruebas de UsuarioB.java , se hacen 2 pruebas diferentes
+ * misma prueba pero con diferentes numeros, se crea un ArrayList
+ * con ciertos numeros (pequeños y grandes)  se crea una variable
+ * y se la pasa el valor del metodo UsuarioB.java.
+ * Despues comprobaremos el resultado esperado con assertEquals
+ * verde-> correcto negro-> malo
+ * @author andexo
+ *
+ */
 class UsuariobTest {
 
 	@Test
-	
+	/**
+	 * Metodo que comprueba el el valor recodigo con el esperado,
+	 * crea un ArrayList , pasa unos numeros , crea una variable y 
+	 * se pasa el valor del metodo en la clase UsuarioB.java 
+	 * y comprobamos con assertEquals
+	 */
 	public void result30() {
 		ArrayList<Integer> numeros = new ArrayList<Integer>();
 		numeros.add(30);
@@ -24,7 +38,12 @@ class UsuariobTest {
 	
 	
 	@Test
-	
+	/**
+	 * Metodo que comprueba el el valor recodigo con el esperado,
+	 * crea un ArrayList , pasa unos numeros , crea una variable y 
+	 * se pasa el valor del metodo en la clase UsuarioB.java 
+	 * y comprobamos con assertEquals
+	 */
 	public void result402() {
 		ArrayList<Integer> numeros = new ArrayList<Integer>();
 		numeros.add(50);
